975946cca02037314b802eba74dca34e1c24ad58
josh handelman <71081051+handelmanjosh@users.noreply.github.com>
2023-03-12 16:07:03 -0400

    Revert "initial"
    This reverts commit 26b9d32dc5469b98c0615ae7e22adb0e831926fb.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,24 @@
-
-import { useEffect, useState } from 'react';
+import React from 'react';
+import logo from './logo.svg';
 import './App.css';
-import Player from './components/player';
-import Sidebar from './components/sidebar';
 
 function App() {
-  const [isPhone, setIsPhone]= useState<boolean>(false);
-
-  //will not handle dynamic resizing, but that should not be necessary. 
-  useEffect(() => {
-    if (window.innerWidth < 768) {
-      setIsPhone(true);
-    }
-  }, []);
   return (
-    <div className="flex flex-col justify-center items-center bg-red-600 w-full h-[100vh]"> 
-      <div className="flex flex-row justify-center items-center bg-slate-600 w-full h-full">
-        <Sidebar />
-        <div className="relative w-[80%] md:w-[60%] md:h-[50%] h-[90%] bg-green-600">
-          {isPhone 
-          ? 
-          <> 
-            <div className="absolute flex flex-col items-center justify-center left-[-10%] bg-yellow-400 h-full">
-              <Player />
-              <Player />
-            </div>
-            <div className="absolute flex flex-col items-center justify-center left-[100%] bg-yellow-400 h-full">
-              <Player />
-              <Player />
-            </div>
-          </> 
-          : <>
-              <Player className="left-[50%] top-[-10%]" />
-              <Player className="left-[50%] top-[100%]" />
-              <Player className="left-[-10%] top-[50%]" />
-              <Player className="left-[100%] top-[50%]" />
-            </>}
-        </div>
-        <Sidebar />
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
